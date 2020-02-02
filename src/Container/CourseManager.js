a434adc28a410903ea3989eadca261682f125455
Xi Zheng <xizheng@Xis-MacBook-Pro.local>
2020-02-02 15:24:47 -0500

    Assignment 3 Create Course Manager, Constants

---
+++ src/Container/CourseManager.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+
+export default class CourseManager
+    extends Component{
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>Course Manager</h1>
+            </div>
+        )
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CourseManager from './Container/CourseManager';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(
+    <CourseManager/>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
