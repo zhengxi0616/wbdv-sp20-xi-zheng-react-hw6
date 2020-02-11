6b9316f7acaa6727500f83e3844845f150aeb005
Xi Zheng <xizheng@Xis-MacBook-Pro.local>
2020-02-11 00:15:19 -0500

    Assignment 4 ModuleListComponent

--- src/components/CourseEditor/ModuleListComponent.js
+++ src/components/CourseEditor/ModuleListComponent.js
@@ -10,7 +10,6 @@ export default class ModuleListComponent extends React.Component {
         activeModuleId: this.props.moduleId,
         editingModuleId: '',
         EditedModuleTitle: '',
-        NewModuleTile:'New Module'
     }
 
     render() {
@@ -28,10 +27,7 @@ export default class ModuleListComponent extends React.Component {
                             {module._id !== this.state.editingModuleId &&
                             <span>{module.title}
                             <div className={"float-right"}>
-                                <button onClick={() =>this.props.deleteModule(module._id)}
-                                    className="float-right">
-                                    <i className="far fa-window-close"></i>
-                                    </button>
+
                                 <button onClick={() => {
                                     const moduleId = module._id
                                     this.props.history.push(`/course/${this.props.courseId}/module/${moduleId}`)
@@ -57,7 +53,10 @@ export default class ModuleListComponent extends React.Component {
                                 <div className={"float-right"}>
                                     <button onClick={() => this.props.updateModule(module._id, {title: this.state.EditedModuleTitle})
                                         .then(() => this.setState({editingModuleId: ''}))}>
-                                    <i className="fas fa-save"></i>
+                                        <i className="fas fa-save"></i>
+                                    </button>
+                                    <button onClick={() =>this.props.deleteModule(module._id)}>
+                                        <i className="far fa-window-close"></i>
                                     </button>
                                 </div>
                                 </span>}
@@ -67,14 +66,9 @@ export default class ModuleListComponent extends React.Component {
 
                 }
                 <li className="list-group-item">
-                    <input
-                        onChange={(e) => this.setState({
-                            NewModuleTile: e.target.value
-                        })}
-                        value={this.state.NewModuleTile}/>
-                    <div className={"float-right"}>
+                    <div className={"col-7 float-right"}>
                         <button onClick={
-                            () => this.props.createModule(this.props.courseId, {title: this.state.NewModuleTile})
+                            () => this.props.createModule(this.props.courseId, {title: "New Module"})
                         }>
                             <i className="fas fa-plus"></i>
                         </button>
