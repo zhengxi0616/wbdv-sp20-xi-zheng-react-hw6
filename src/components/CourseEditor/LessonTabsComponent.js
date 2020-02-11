105afe264f14435f17ca97286960c8ad848e078b
Xi Zheng <xizheng@Xis-MacBook-Pro.local>
2020-02-10 23:20:13 -0500

    Assignment 4 Edit Lesson Container and Component

--- src/components/CourseEditor/LessonTabsComponent.js
+++ src/components/CourseEditor/LessonTabsComponent.js
@@ -1,9 +1,6 @@
 import React from "react";
-import {connect} from "react-redux";
-import {LESSONS_API_URL, MODULES_LESSONS_API_URL} from "../../Common/Constants";
-import {updateLesson} from "../../services/LessonService";
 
-class LessonTabsComponent extends React.Component {
+export default class LessonTabsComponent extends React.Component {
 
     componentDidMount() {
         this.props.findLessonsForModule(this.props.moduleId)
@@ -21,7 +18,9 @@ class LessonTabsComponent extends React.Component {
         lesson: {
             title: '',
             _id: ''
-        }
+        },
+        EditLessonTitle: '',
+        NewLessonTile:'New Module'
     }
 
     render() {
@@ -83,61 +82,3 @@ class LessonTabsComponent extends React.Component {
     }
 }
 
-
-const stateToPropertyMapper = (state) => ({
-    lessons: state.lessonReducer.lessons
-})
-
-const dispatcherToPropertyMapper = (dispatcher) => ({
-    findLessonsForModule: moduleId =>
-        fetch(MODULES_LESSONS_API_URL(moduleId))
-            .then(response => response.json())
-            .then(lessons => dispatcher({
-                type: 'FIND_LESSONS_FOR_MODULE',
-                lessons: lessons
-            })),
-    updateLesson: async (lesson) => {
-        const actualLesson = await updateLesson(lesson)
-        dispatcher({
-            type: 'UPDATE_LESSON',
-            lesson: actualLesson,
-            lessonId: actualLesson._id
-        })
-    },
-    addLesson: (moduleId) =>
-        fetch(MODULES_LESSONS_API_URL(moduleId), {
-            method: 'POST',
-            body: JSON.stringify({title: 'New Lesson'}),
-            headers: {
-                'content-type': 'application/json'
-            }
-        }).then(response => response.json())
-            .then(actualLesson =>
-                dispatcher({
-                    type: 'CREATE_LESSON',
-                    lesson: actualLesson
-                })),
-    deleteLesson: (lessonId) =>
-        fetch(`${LESSONS_API_URL}/${lessonId}`, {
-            method: 'DELETE'
-        }).then(response => response.json())
-            .then(status =>
-                dispatcher({
-                    type: 'DELETE_LESSON',
-                    lessonId: lessonId
-                })),
-    findAllLessons: () =>
-        fetch(LESSONS_API_URL)
-            .then(response => response.json())
-            .then(lessons =>
-                dispatcher({
-                    type: 'FIND_ALL_LESSONS',
-                    lessons: lessons
-                })
-            )
-})
-
-export default connect(
-    stateToPropertyMapper,
-    dispatcherToPropertyMapper
-)(LessonTabsComponent)
--- src/containers/LessonTabsContainer.js
+++ src/containers/LessonTabsContainer.js
@@ -1,62 +1,50 @@
-import React from "react"
-import {LESSONS_API_URL, MODULES_LESSONS_API_URL} from "../Common/Constants";
-import service from "../services/LessonService";
+import React from "react";
 import {connect} from "react-redux";
+import service from "../services/LessonService";
+import LessonTabsComponent from "../components/CourseEditor/LessonTabsComponent";
+
 
 const stateToPropertyMapper = (state) => ({
     lessons: state.lessonReducer.lessons
 })
 
-const dispatcherToPropertyMapper = (dispatcher) => ({
-    findLessonsForModule: moduleId =>
-        fetch(MODULES_LESSONS_API_URL(moduleId))
-            .then(response => response.json())
-            .then(lessons => dispatcher({
-                type: 'FIND_LESSONS_FOR_MODULE',
-                lessons: lessons
-            })),
-    updateLesson: async (lesson) => {
-        const actualLesson = await updateLesson(lesson)
-        dispatcher({
-            type: 'UPDATE_LESSON',
-            lesson: actualLesson,
-            lessonId: actualLesson._id
-        })
-    },
-    addLesson: (moduleId) =>
-        fetch(MODULES_LESSONS_API_URL(moduleId), {
-            method: 'POST',
-            body: JSON.stringify({title: 'New Lesson'}),
-            headers: {
-                'content-type': 'application/json'
-            }
-        }).then(response => response.json())
+const dispatchToPropertyMapper = (dispatch) => ({
+    createLesson: (moduleId, lesson) =>
+        service.createLesson(moduleId, lesson)
             .then(actualLesson =>
-                dispatcher({
-                    type: 'CREATE_LESSON',
+                dispatch({
+                    type: "CREATE_LESSON",
                     lesson: actualLesson
                 })),
-    deleteLesson: (lessonId) =>
-        fetch(`${LESSONS_API_URL}/${lessonId}`, {
-            method: 'DELETE'
-        }).then(response => response.json())
-            .then(status =>
-                dispatcher({
-                    type: 'DELETE_LESSON',
-                    lessonId: lessonId
-                })),
-    findAllLessons: () =>
-        fetch(LESSONS_API_URL)
-            .then(response => response.json())
+    findLessonsForModule: (moduleId) =>
+        service.findLessonsForModule(moduleId)
             .then(lessons =>
-                dispatcher({
-                    type: 'FIND_ALL_LESSONS',
-                    lessons: lessons
-                })
-            )
+                dispatch({
+                    type: "FIND_LESSONS_FOR_MODULE",
+                    modules: lessons
+                })),
+
+    deleteLesson: (lessonId) =>
+        service.deleteLesson(lessonId)
+            .then(status =>
+                dispatch({
+                    type: "DELETE_LESSON",
+                    moduleId: lessonId
+                })),
+
+    updateLesson: (lessonId, lesson) =>
+        service.updateLesson(moduleId,lesson)
+            .then(status =>
+                dispatch({
+                    type: "UPDATE_LESSON",
+                    module: lesson,
+                    moduleId: lessonId
+                }))
 })
 
-export default connect(
+const LessonTabsContainer = connect(
     stateToPropertyMapper,
-    dispatcherToPropertyMapper
+    dispatchToPropertyMapper
 )(LessonTabsComponent)
+
+export default LessonTabsContainer
